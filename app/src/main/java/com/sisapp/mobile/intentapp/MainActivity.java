cffa089ef8cf0193d1d5046b30b901fef73a97e4
mftlhn <miftakhussolikhin@gmail.com>
2020-02-20 17:55:38 +0700

    - lanjut

--- app/src/main/java/com/sisapp/mobile/intentapp/MainActivity.java
+++ app/src/main/java/com/sisapp/mobile/intentapp/MainActivity.java
@@ -1,15 +1,18 @@
 package com.sisapp.mobile.intentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,6 +22,8 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +45,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("Google Pixel");
         }
+
+        imageView = findViewById(R.id.image_action);
+
     }
-
-
 
 
     @Override
@@ -65,8 +71,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btn_camera:
                 Intent cam = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (cam.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(cam, REQUEST_IMAGE_CAPTURE);
+                if (cam != null && cam.resolveActivity(getPackageManager()) != null) {
+//                    startActivityForResult(cam, REQUEST_IMAGE_CAPTURE);
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException e) {
+
+                    }
+                    if (photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+//                        photoURI = Uri.fromFile()
+                        cam.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(cam, REQUEST_TAKE_PHOTO);
+                    }
                 }
                 break;
         }
@@ -77,6 +95,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
         }
     }
 
@@ -96,5 +115,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Save a file: path for use with ACTION_VIEW intents
         currentPhotoPath = image.getAbsolutePath();
         return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        imageView.setImageBitmap(bitmap);
     }
 }
