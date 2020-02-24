161010f6837d603b1327586e46b6f5ee96a67d89
mftlhn <miftakhussolikhin@gmail.com>
2020-02-24 13:43:10 +0700

    - done

--- app/src/main/java/com/sisapp/mobile/intentapp/RecyclerViewClass.java
+++ app/src/main/java/com/sisapp/mobile/intentapp/RecyclerViewClass.java
@@ -1,32 +1,89 @@
 package com.sisapp.mobile.intentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.sisapp.mobile.intentapp.adapter.CardViewHeroAdapter;
+import com.sisapp.mobile.intentapp.adapter.GridHeroAdapter;
 import com.sisapp.mobile.intentapp.adapter.ListHeroAdapter;
+import com.sisapp.mobile.intentapp.model.Hero;
+import com.sisapp.mobile.intentapp.model.HeroesData;
+
+import java.util.ArrayList;
 
 public class RecyclerViewClass extends AppCompatActivity {
     private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
+    private String title = "Mode List";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view_class);
+        setActionBarTitle(title);
 
         rvHeroes = findViewById(R.id.rv_heroes);
         rvHeroes.setHasFixedSize(true);
 
-        rvHeroes = findViewById(R.id.rv_heroes);
-        rvHeroes.setHasFixedSize(true);
+        list.addAll(HeroesData.getListData());
+        showRecyclerList();
     }
 
     private void showRecyclerList(){
         rvHeroes.setLayoutManager(new LinearLayoutManager(this));
         ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
         rvHeroes.setAdapter(listHeroAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+    public void setMode(int selectedMode) {
+        switch (selectedMode) {
+            case R.id.action_list:
+                title = "Mode List";
+                showRecyclerList();
+                break;
+            case R.id.action_grid:
+                showRecyclerGrid();
+                title = "Mode Grid";
+                break;
+            case R.id.action_cardview:
+                showRecyclerCardView();
+                title = "Mode CardView";
+                break;
+        }
+    }
+
+    private void showRecyclerGrid(){
+        rvHeroes.setLayoutManager(new GridLayoutManager(this, 2));
+        GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
+        rvHeroes.setAdapter(gridHeroAdapter);
+    }
+
+    private void showRecyclerCardView(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        CardViewHeroAdapter cardViewHeroAdapter = new CardViewHeroAdapter(list);
+        rvHeroes.setAdapter(cardViewHeroAdapter);
+    }
+
+    private void setActionBarTitle(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
     }
 
 }
